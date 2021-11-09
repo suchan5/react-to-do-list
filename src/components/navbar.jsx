6b77d542a8d0c378117cfde29e2fe3e7d124d611
suchan5 <suchan5@hotmail.com>
2021-11-09 13:07:08 +0800

    updated the title

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ class Navbar extends Component {  //Navbar로 대문자로 바꿔주고
         return (
             <nav className="navbar">
                 <i className="navbar-logo fas fa-leaf"></i>
-                <span>Habit Tracker</span>
+                <span>To Do List</span>
                 <span className="navbar-count">{this.props.totalCount}</span>
             </nav>
         );
